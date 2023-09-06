a6618e27e3defb9e96f8b7e2499aa6db9c237c9c
Fernando Poblete Arrau <fernandopobletearrau@gmail.com>
2023-09-06 12:53:43 -0300

    Se crea funcion que permite ir sobreescribiendo multiples frames.

--- create_pdf.py
+++ create_pdf.py
@@ -8,6 +8,15 @@ from reportlab.lib.styles import getSampleStyleSheet
 from io import BytesIO
 
 def create_pdf(input_pdf, output_pdf, text, image_path, x, y):
+    new_output_pdf = output_pdf + "_2"
+    img = build_image(image_path, 400, 75)
+    overlay_frame(input_pdf + '.pdf', output_pdf, img, 410, 90, x, y)
+
+    p = build_paragraph(text)
+    overlay_frame(output_pdf + '.pdf', new_output_pdf, p, 150, 50, x + 210, y)
+
+
+def overlay_frame(input_pdf, output_pdf, element, width, height, x, y):
     # Create a buffer to hold the modified PDF content
     buffer = BytesIO()
 
@@ -19,18 +28,12 @@ def create_pdf(input_pdf, output_pdf, text, image_path, x, y):
     pdf = SimpleDocTemplate(buffer, pagesize=letter)
     pdf.pages = []
 
-    frame = Frame(x, y, pdf.width * 0.7 , pdf.height / 5, id="normal")
-    frame.showBoundary = True
-    
+    frame = Frame(x, y, width, height, id="normal")
+    # frame.showBoundary = True
+
     template = PageTemplate(id="test", frames=[frame])
     story = []
-    
-    p = build_paragraph(text)
-    story.append(p)
-
-    img = build_image(image_path)
-    story.append(img)
-
+    story.append(element)
     # Build the PDF with the Story and PageTemplate
     pdf.addPageTemplates([template])
     pdf.build(story)
@@ -43,24 +46,24 @@ def create_pdf(input_pdf, output_pdf, text, image_path, x, y):
         output_pdf_writer.addPage(page)
 
     # Write the modified content to the output PDF file
-    with open(output_pdf, "wb") as f:
+    with open(output_pdf + ".pdf", "wb") as f:
         output_pdf_writer.write(f)
+
 
 def build_paragraph(text):
     # Set the font and font size
     styles = getSampleStyleSheet()
     style = styles['Normal']
     style.fontName = 'Helvetica'
-    style.fontSize = 10
+    style.fontSize = 15
     # Create a Paragraph with the dynamic text
     p = Paragraph(text, style)
     p.hAlign = 'RIGHT'
     p.vAlign = 'BOTTOM'
     return p
 
-def build_image(image_path):
-    #img = Image(image_path, width=450, height=75)
-    img = Image(image_path, 400, 50)
+def build_image(image_path, width, height):
+    img = Image(image_path, width, height)
     img.hAlign = 'LEFT'
     img.vAlign = 'BOTTOM'
     return img
@@ -70,11 +73,11 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: python3 create_pdf.py output_pdf dynamic_text")
     else:
-        input_pdf = "pdfs/empty.pdf" # "pdfs/document.pdf"
+        input_pdf = "pdfs/empty" # "pdfs/document.pdf"
         x_coordinate = 0
         y_coordinate = 10
         image_path = "imgs/footer.jpg"
 
-        output_pdf = "pdfs/generated/" + sys.argv[1] + ".pdf"
+        output_pdf = "pdfs/generated/" + sys.argv[1]
         dynamic_text = sys.argv[2]
         create_pdf(input_pdf, output_pdf, dynamic_text, image_path, x_coordinate, y_coordinate)
