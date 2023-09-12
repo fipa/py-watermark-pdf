f4ed4fc300c3ad701da6279792f208ce3bc4a22d
Fernando Poblete Arrau <fernandopobletearrau@gmail.com>
2023-09-12 16:24:31 -0300

    Se agrega ahora en un solo momento la imagen y el texto, evitando la creacion de dos pdfs.

--- create_pdf.py
+++ create_pdf.py
@@ -9,79 +9,48 @@ from reportlab.pdfbase.ttfonts import TTFont
 
 from io import BytesIO
 
+MILIMITERS_TO_POINTS = 72 / 25.4
+
 def create_pdf(input_pdf, output_pdf, text, email, image_path, x, y):
-    tmp_output_pdf = output_pdf.replace(".pdf", "_tmp.pdf")
     img_resize = 0.18
     width = 1506 * img_resize
     height = 93 * img_resize
-    frame_resize = 3
+    frame_resize = 2
     img = build_image(image_path, width, height)
-    # overlay_frame(input_pdf, tmp_output_pdf, img, width + 6, height * frame_resize, x, y)
 
     p = build_paragraph(text, email, 'ShadowsIntoLight-Regular')
-    #overlay_frame(tmp_output_pdf, output_pdf, p, 300, height * frame_resize * 0.8, x + 140, y + 3)
-    new_overlay(input_pdf, output_pdf, img, p, width + 6, height * frame_resize, x, y)
+    overlay(input_pdf, output_pdf, img, p, width + 6, height * frame_resize, x, y)
 
-def new_overlay(input_pdf_path, output_pdf, img, p, width, height, x, y):
+def overlay(input_pdf_path, output_pdf, img, p, width, height, x, y):
     buffer = BytesIO()
 
     existing_pdf = PdfFileReader(open(input_pdf_path, "rb"))
     output_pdf_writer = PdfFileWriter()
 
-    pdf = SimpleDocTemplate(buffer, pagesize=letter)
+    page_width = 283 * MILIMITERS_TO_POINTS
+    page_height = 216 * MILIMITERS_TO_POINTS
+
+    pdf = SimpleDocTemplate(buffer, pagesize=(page_width, page_height))
     pdf.pages = []
 
-    frame_width = width  # Divide the page width in half
+    frame_width = pdf.width * 0.45  # Divide the page width in half
     frame_img = Frame(x, y, frame_width, height, id="img_frame")
     frame_img.showBoundary = True
     frame_text = Frame(x + frame_width, y, frame_width, height, id="text_frame")
     frame_text.showBoundary = True
 
-    available_space = frame_text.height - p.wrap(frame_text.width, frame_text.height)[1]  # Height of the text frame minus wrapped paragraph height
-    spacer = Spacer(1, available_space) if available_space > 0 else Spacer(1, 1)  # Spacer to take up available space, but ensure it's at least 1 unit high
-
     template = PageTemplate(id="custom_template", frames=[frame_img, frame_text])
-    story = [img, spacer, p]  # Use Spacer to separate image and text
-    pdf.addPageTemplates([template])
-    pdf.build(story)
-
-    output_pdf_writer.addPage(existing_pdf.getPage(0))
-    # for i in range(1, existing_pdf.getNumPages()):
-    for i in range(1, 3):
-        page = existing_pdf.getPage(i)
-        overlay_page = PdfFileReader(buffer).getPage(0)
-        page.mergeTranslatedPage(overlay_page, x, y)
-        output_pdf_writer.addPage(page)
-    output_pdf_writer.addPage(existing_pdf.getPage(existing_pdf.getNumPages() - 1))
-
-    with open(output_pdf, "wb") as f:
-        output_pdf_writer.write(f)
-
-def overlay_frame(input_pdf, output_pdf, element, width, height, x, y):
-    buffer = BytesIO()
-
-    existing_pdf = PdfFileReader(open(input_pdf, "rb"))
-    output_pdf_writer = PdfFileWriter()
-
-    pdf = SimpleDocTemplate(buffer, pagesize=letter)
-    pdf.pages = []
-
-    frame = Frame(x, y, width, height, id="normal")
-    # frame.showBoundary = True
-
-    template = PageTemplate(id="test", frames=[frame])  
-    story = []
-    story.append(element)
+    story = [img, p]
     pdf.addPageTemplates([template])
     pdf.build(story)
 
     output_pdf_writer.addPage(existing_pdf.getPage(0))
     for i in range(1, existing_pdf.getNumPages() - 1):
         page = existing_pdf.getPage(i)
-        overlay_page =   PdfFileReader(buffer).getPage(0)
+        overlay_page = PdfFileReader(buffer).getPage(0)
         page.mergeTranslatedPage(overlay_page, x, y)
         output_pdf_writer.addPage(page)
-    output_pdf_writer.addPage(existing_pdf.getPage(existing_pdf.getNumPages() - 1))        
+    output_pdf_writer.addPage(existing_pdf.getPage(existing_pdf.getNumPages() - 1))
 
     with open(output_pdf, "wb") as f:
         output_pdf_writer.write(f)
