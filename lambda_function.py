784a4c082021dbdf2d21a7b2532c6d9db4cd8468
Fernando Poblete Arrau <fernandopobletearrau@gmail.com>
2023-09-18 02:12:22 -0300

    Changes for S3 integration.

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,9 @@
+import boto3
+from botocore.exceptions import NoCredentialsError
+
 import generate
+
+s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
     action = event["action"]
@@ -11,16 +16,41 @@ def lambda_handler(event, context):
             }
         }
     
-    input_pdf = "pdfs/" + event["input_pdf"] + ".pdf"
+    input_bucket = "bitacoraemocional"
+    input_key = "pdfs/" + event["input_pdf"] + ".pdf"
+    output_bucket = "bitacoraemocional"
+    output_key = "pdfs/generated/" + event["input_pdf"] + "_" + event["text"].replace(" ", "") + ".pdf"
+
+    try:
+        s3.download_file(input_bucket, input_key, '/tmp/input.pdf')
+    except NoCredentialsError:
+        return {
+            "statusCode": 500,
+            "body": {
+                "error": "Unable to access S3. Check AWS credentials to read."
+            }
+        }
+
+    input_pdf = "/tmp/input.pdf"
     x_coordinate = 0
     y_coordinate = 0
     image_path = "imgs/footer.jpg"
     
     text = event["text"]
-    output_pdf = "pdfs/generated/" + event["input_pdf"] + "_" + text.replace(" ", "") + ".pdf"
+    output_pdf = "/tmp/output.pdf"
     email = event["email"]
     last_page_to_merge = int(event["last_page"]) if "last_page" in event else None
     generate.create_pdf(input_pdf, output_pdf, text, email, image_path, x_coordinate, y_coordinate, last_page_to_merge)
+
+    try:
+        s3.upload_file('/tmp/output.pdf', output_bucket, output_key)
+    except NoCredentialsError:
+        return {
+            "statusCode": 500,
+            "body": {
+                "error": "Unable to upload to S3. Check AWS credentials to write."
+            }
+        }
 
     return {
         "statusCode": 200,
